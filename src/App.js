b23db55aefde5a064aa69b3e0a7552e698a67e2b
nick-hiebl <nicholas.hiebl@gmail.com>
2022-06-12 13:11:09 +1000

    Change default route

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import React from 'react'
-import { BrowserRouter, Route, Routes, useParams } from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 import MultiWordle from './pages/MultiWordle'
 import Tournament from './pages/Tournament'
 
 import './App.css'
 
-function Tester() {
-  const params = useParams()
-
-  return <h1>Params: {params.ext}</h1>
-}
-
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path=":ext" elements={<Tester />} />
-        <Route path="tournament" element={<Tournament />} />
-        <Route path="*" element={<MultiWordle />} />
+        <Route path="*" element={<Tournament />} />
+        <Route path="multi" element={<MultiWordle />} />
       </Routes>
     </BrowserRouter>
   )
