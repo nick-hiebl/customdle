5e7f7da15d5ef3fb59ee7c9ad73c7f1f7fc06644
nick-hiebl <nicholas.hiebl@gmail.com>
2022-06-12 13:01:29 +1000

    Add testing code

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import React from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Route, Routes, useParams } from 'react-router-dom'
 
 import MultiWordle from './pages/MultiWordle'
 import Tournament from './pages/Tournament'
 
 import './App.css'
 
+function Tester() {
+  const params = useParams()
+
+  return <h1>Params: {params.ext}</h1>
+}
+
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path=":ext" elements={<Tester />} />
         <Route path="tournament" element={<Tournament />} />
         <Route path="*" element={<MultiWordle />} />
       </Routes>
